33fc80ffe6a6abefe85656d3deca3d0a3c301a5e
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2019-10-04 21:15:20 +0300

    Updates

---
+++ precache-manifest.1c2f5d5fe832b4f1cce83860f6100df2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fddd306e989ed4064cb11ece1afbebc",
+    "url": "/goit-react-hw-01-components/./index.html"
+  },
+  {
+    "revision": "eb042281e31652c31718",
+    "url": "/goit-react-hw-01-components/./static/css/main.0504b8cc.chunk.css"
+  },
+  {
+    "revision": "1f7801f52614985c64bb",
+    "url": "/goit-react-hw-01-components/./static/js/2.a3570416.chunk.js"
+  },
+  {
+    "revision": "eb042281e31652c31718",
+    "url": "/goit-react-hw-01-components/./static/js/main.cec9b8d6.chunk.js"
+  },
+  {
+    "revision": "1d66c37e194aa0f3d21c",
+    "url": "/goit-react-hw-01-components/./static/js/runtime-main.93ed941f.js"
+  }
+]);
